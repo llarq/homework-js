a19e86b40513cf6be22dc926cd4e70bf75800900
Sergei <s.leushkin1509@gmail.com>
2025-01-28 20:39:48 +0200

    first trial js

---
+++ JS/homework1.js
@@ -0,0 +1,60 @@
+const age = 15;
+console.log(age);
+
+const name = "Serhii";
+console.log(name);
+
+const isStudent = false;
+console.log(isStudent);
+
+const myString = "Ви пам’ятаєте, скільки хліба ви з’їли за все життя?";
+console.log(myString);
+
+let myNumber = 10
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+const yourName = prompt("Введіть ваше ім’я");
+const userName = alert("Привіт " + yourName);
+
+const agreeMessage = confirm("Підтвердіть дію");
+if (agreeMessage === true) {
+    alert("Дякую за підтвердження!")
+}
+else {
+    alert("Дію відмінено!")
+};
+
+const warningMessage = alert("Дія небезпечна, потрібне підтвердження");
+const agreeWarning = confirm("Підтвердити дію");
+if (agreeWarning === true) {
+    alert("Дякую за підтвердження!")
+ }
+ else {
+     alert("Дію відмінено!")
+ };
+
+
+
+
+// let warningMessage = alert("Дія небезпечна, потрібне підтвердження");
+// warningMessage = confirm("Підтвердіть дію");
+// if (warningMessage === true) {
+//     alert = ("Дякую за підтвердження!")
+// }
+// else {
+//     alert = ("Дію відмінено!")
+// } ;
+
+
+// const warningMessage = alert("Дія небезпечна, потрібне підтвердження");
+// const agreeAlert = confirm("Підтвердіть дію");
+// if (agreeAlert === true) {
+//     alert = ("Дякую за підтвердження!")
+// }
+// else {
+//     alert = ("Дію відмінено!")
+// };
