4dafef41bdeb773d5265467bfd3f5b0f74496289
Sergei <s.leushkin1509@gmail.com>
2025-02-13 18:40:46 +0200

    complete homework 2, 3

---
+++ JS/homework2.js
@@ -0,0 +1,58 @@
+// const celsius = prompt("Enter the number of degrees");
+// const fahrenheit = celsius * 1.8 + 32;
+// console.log(fahrenheit);
+
+// const daysInMonth = prompt('Enter the number of days');
+// const hoursInMonth = daysInMonth * 24;
+// const minutesInMonth = daysInMonth * 1440;
+// console.log(hoursInMonth + " " + "hours");
+// console.log(minutesInMonth + " " + "minutes")
+
+// let health = 100;
+// let energy = 1000;
+// health -= 45;
+// energy -= 700;
+// console.log(health);
+// console.log(energy);
+
+// const totalPrice = prompt('Enter the purchase amount');
+// const discount = 0.1;
+// const discountedPrice = totalPrice - (totalPrice * discount);
+// console.log(discountedPrice);
+
+// const floatNumber = 12.78;
+// console.log(Math.floor(floatNumber))
+
+// const floatString = '45.67';
+// console.log(parseFloat(floatString));
+
+// const intString = '123';
+// console.log(parseInt(intString));
+
+// const number = prompt("Enter the number");
+// const sqrtNumber = Math.sqrt(number);
+// console.log(sqrtNumber);
+
+// const stringNumber = '256';
+// const convertedstring = parseInt(stringNumber);
+// console.log(convertedstring);
+
+// ---------------------------------
+
+// console.log(Math.max(10, 58, 39, -150, 0));
+
+// console.log(Math.floor(58.858));
+
+// const maxNumber = 100;
+// const minNumber = 1;
+// const randomNumber = Math.random() * (maxNumber - minNumber) + minNumber;
+// console.log(Math.floor(randomNumber));
+
+
+
+
+
+
+
+
+
---
+++ JS/homework3.js
@@ -0,0 +1,29 @@
+// const result = 5 + 5 + '5';
+// console.log(result);
+
+// const userEmail = "llarqeh@gmail.com";
+// const userEmailCheck = userEmail.includes("@");
+// console.log(userEmailCheck, userEmail.length);
+
+// const firstWord = "my"
+// const secondWord = "name"
+// const thirdWord = "is"
+// const fullName = firstWord + " " + secondWord + " " + thirdWord + " " + "Victor";
+// console.log(fullName);
+
+// const userName = "Nikita";
+// const payment = 700;
+// alert(`Дякуємо, ${userName}. До сплати ${payment} гривень`);
+
+// const myName = "Serhii Leushkin";
+// console.log(myName.indexOf(" "));
+
+// const blacklistedWord1 = 'спам'
+// const blacklistedWord2 = 'розпродаж'
+// const string1 = 'Привіт, я Ілон Маск, це не спам, пропоную тобі мільйон!'
+// const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!'
+// const string3 = 'Рекламна кампанія #jsIsAwesome'
+
+// console.log(string1.includes(blacklistedWord1.toLowerCase()));
+// console.log(string1.includes(blacklistedWord2.toLowerCase()));
+// console.log(string2.includes(blacklistedWord2.toUpperCase()));
