40dc1433dc1b6ad1a40c6ece28528d70a316c764
Sergei <s.leushkin1509@gmail.com>
2025-02-16 13:44:44 +0200

    complete 4 homework

---
+++ JS/homework4.js
@@ -0,0 +1,45 @@
+// const text1 = "asda";
+// const text2 = "hello";
+// if (text1 !== "" && text2 !== "") {
+//   console.log('Обидва поля заповнені')
+// }
+// else {
+//   console.log('Не всі поля заповнені')  
+// }
+
+// const number1 = 4;
+// const number2 = 5;
+// const sum = number1 + number2;
+// if (sum > 10) {
+//   console.log('Сума більша за 10')
+// }
+// else if (sum <= 10) {
+//   console.log("Сума менша або дорівнює 10");
+// };
+
+// const text3 = "JavaScript";
+// if (text3.includes("JavaScript")) {
+//   console.log('Текст містить слово JavaScript')
+// }
+// else {
+//   console.log('Текст не містить слово JavaScript')
+// };
+
+// const number3 = 15;
+// if (number3 > 10 && number3 < 20) {
+//   console.log("Число входить в діапазон від 10 до 20");
+// }
+// else { 
+//   console.log('Число не входить в діапазон від 10 до 20') 
+// }
+
+const email = "llarqeh@gmail.com";
+const userName = "Serhii";
+const pass = "1233123";
+if (email.includes("@") && email.includes(".", email.indexOf("@")) && userName.length > 3 && pass.length > 6) {
+  console.log('Перенаправлення на іншу сторінку')
+}
+else {
+  console.log('Помилка: неправильне заповнення')
+};
+
