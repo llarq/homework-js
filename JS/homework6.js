76c2586f977fe0fe974022ad2d7dfebebe1a8c66
Sergei <s.leushkin1509@gmail.com>
2025-03-09 13:03:44 +0200

    6

---
+++ JS/homework6.js
@@ -0,0 +1,36 @@
+// let number = 0;
+// while (number < 10) {
+//   number += 1;
+//   console.log(number)
+// }
+
+// let num = 20;
+// for (let i = 0; i <= num; i += 1) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i)
+// }
+
+
+// let num1 = 7;
+// for (let i = 0; i <= 10; i += 1) {
+//   console.log(i * num1);
+// }
+
+
+// num2 = 6
+// for (let i = 0; i <= 10; i += 1) {
+//   if (i >= num2) {
+//     break;
+//   }
+//   console.log(i); 
+// }
+
+let num3 = 1;
+while (num3 <= 20) {
+  if (num3 % 3 !== 0) {
+    console.log(num3)
+  }
+  num3 += 1;
+}
